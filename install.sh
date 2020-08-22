082a76ba9b2a4a54d1a47af07c60868fbcfa21f1
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-08-22 15:54:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "--        Creating mirrorlist backup           --"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sed -e 's/^#Server/Server/g' > /etc/pacman.d/mirrorlist
 pacman -Syy
 
 
