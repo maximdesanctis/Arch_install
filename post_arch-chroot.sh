fcfda331d37f054dc9b7c4d2dbe72510e7fd1c66
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-08-23 16:11:08 +0200

    Update post_arch-chroot.sh

--- post_arch-chroot.sh
+++ post_arch-chroot.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
+
+echo "--------------------------------------"
+echo "--  Bootloader Grub Installation    --"
+echo "--------------------------------------"
+pacman -S grub efibootmgr efivar --noconfirm
+lsblk
+echo "Please enter disk to install grub on: (example /dev/sda)"
+read DISK
+grub-install "${DISK}"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager wpa_supplicant wireless_tools --noconfirm --needed
 systemctl enable NetworkManager
