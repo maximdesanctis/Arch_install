80043d469af370ce78d9068c40160c13e77a9237
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-08-23 16:48:21 +0200

    Update post_arch-chroot.sh

--- post_arch-chroot.sh
+++ post_arch-chroot.sh
@@ -3,7 +3,7 @@
 echo "--------------------------------------"
 echo "--  Bootloader Grub Installation    --"
 echo "--------------------------------------"
-pacman -S grub efibootmgr efivar --noconfirm
+pacman -S grub efibootmgr efivar intel-ucode --noconfirm
 lsblk
 echo "Please enter disk to install grub on: (example /dev/sda)"
 read DISK
@@ -14,7 +14,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo "--------------------------------------"
 echo "--          Network Setup           --"
 echo "--------------------------------------"
-pacman -S networkmanager wpa_supplicant wireless_tools --noconfirm --needed
+pacman -S networkmanager wpa_supplicant wireless_tools dialog --noconfirm --needed
 systemctl enable NetworkManager
 
 
@@ -25,11 +25,17 @@ echo "--------------------------------------"
 pacman -S ufw
 ufw default deny incoming
 ufw default allow outgoing
-systemctl ufw enable
+systemctl enable ufw
 sleep 5
 
 
 echo "--------------------------------------"
 echo "--         Software Setup           --"
 echo "--------------------------------------"
-pacman -S git nano vim bash-completion
+pacman -S git nano vim bash-completion gedit
+
+
+echo "--------------------------------------"
+echo "--          Locale Setup            --"
+echo "--------------------------------------"
+nano /etc/locale.gen | sed -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8'
