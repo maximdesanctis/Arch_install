c29b13ab0d665437b28993ac978e8a5397ccde67
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-08-23 19:06:09 +0200

    Update desktop_env_install.sh

--- desktop_env_install.sh
+++ desktop_env_install.sh
@@ -10,7 +10,22 @@ EDITOR=nano visudo | sed -e 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL'
 pacman -S xorg-server mesa nvidia nvidia-utils nvidia-settings nvidia-lts --noconfirm --needed
 
 # install audio
-pacman -S pulsaudio --noconfirm --needed
+pacman -S pulsaudio pulseaudio-alsa --noconfirm --needed
 
-#install bluetooth
+# install bluetooth
 pacman -S bluez bluez-utils --noconfirm --needed
+
+# install kde
+pacman -S plasma-desktop konsole dolphin firefox gedit --noconfirm --needed
+
+# install kde themes
+pacman -S breeze-gtk breeze-kde5 kde-gtk-config --noconfirm --needed
+
+# install kde addons
+pacman -S kdeplasman-addons
+
+# install kde network manager, kde bluetooth manager, kde audio manager
+pacman -S plasma-nm bluedevil plasma-pa --noconfirm --needed
+
+# install additional sofware
+pacman -S kinfocenter spectacle--noconfirm --needed
