b1fee8629a364edad698b7b88db6dc442e284c2f
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-09-01 12:59:20 +0200

    Update encrypted_install.sh

--- encrypted_install.sh
+++ encrypted_install.sh
@@ -54,7 +54,6 @@ echo "--------------------------------------"
 pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware git --noconfirm
 genfstab -U /mnt > /mnt/etc/fstab
 echo "home           ${DISK}3                                    none" >> /mnt/etc/crypttab
-
 clear
 
 arch-chroot /mnt
