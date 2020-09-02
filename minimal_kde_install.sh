e60a27d02e291cc4d1c383161d3d39d6015ac123
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-09-02 12:36:55 +0200

    Update minimal_kde_install.sh

--- minimal_kde_install.sh
+++ minimal_kde_install.sh
@@ -5,7 +5,7 @@ useradd -m jogi
 usermod -aG wheel jogi
 echo "Please enter password for jogi:"
 passwd jogi
-EDITOR=nano visudo | sed "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL"
+EDITOR=nano visudo | sed -e "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL"
 pacman -S xdg-user-dirs --noconfirm --needed
 xdg-user-dirs-update
 
