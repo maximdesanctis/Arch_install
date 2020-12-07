1f95595d82f76fed76a792dc099f956df88c3b43
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-12-07 09:12:13 +0100

    Update maintenance.sh

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -1,5 +1,12 @@
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sed -e "s/^#Server/Server/g" > /etc/pacman.d/mirrorlist
 pacman -Syu
-yay -Syu
 pacman -S pacman-contrib --noconfirm --needed
+paccache -r
+pacman -Rns $(pacman -Qtdq)
 
-paccache -r 
+yay -Syu
+yay -Yc
+
+rm -rf .cache/*
+journalctl --vacuum-time=2weeks
