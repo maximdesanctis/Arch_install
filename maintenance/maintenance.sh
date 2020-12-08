36fe5b659d4a14246f583a8751eaae558a47362b
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2020-12-08 21:14:54 +0100

    Create maintenance.sh

---
+++ maintenance/maintenance.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# update mirrorlist
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo curl -s "https://www.archlinux.org/mirrorlist/?country=DE&protocol=http&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" | sudo sed -e "s/^#Server/Server/g" > sudo /etc/pacman.d/mirrorlist
+
+# update system
+sudo pacman -Syu
+yay -Syu
+
+# clean cached pacman and AUR files
+sudo pacman -S pacman-contrib --noconfirm --needed
+sudo paccache -r
+sudo pacman -Sc
+sudo pacman -Rns $(pacman -Qtdq)
+yay -Yc
+
+# clean chached files and logs
+sudo rm -rf .cache/*
+sudo journalctl --vacuum-time=2weeks
