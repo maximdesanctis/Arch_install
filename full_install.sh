9ed121e4720373ed64113513a35f04dafbe84534
maxobaerchen <71792292+maxobaerchen@users.noreply.github.com>
2020-12-30 16:33:38 +0100

    Update full_install.sh

--- full_install.sh
+++ full_install.sh
@@ -1,3 +1,19 @@
+# WARNING
+# This script was not tested yet in a Virtual Machine nor on a physical computer
+
+
+echo "------------------------------------------"
+echo "--          Installing ArchLinux        --"
+echo "------------------------------------------"
+echo "--      Make sure your device has a     --"
+echo "--      connection to the internet      --"
+echo "------------------------------------------"
+echo "--If it hasn't yet, you probably need to--"
+echo "--reboot after establishing a connection--"
+echo "------------------------------------------"
+echo ""
+
+
 echo "------------------------------------------"
 echo "--            Syncing clock             --"
 echo "------------------------------------------"
@@ -20,8 +36,8 @@ read DISK
 echo "------------------------------------------"
 echo "--              Preparation             --"
 echo "------------------------------------------"
-sgdisk -Z ${DISK}           # destroy existing mbr or gpt structures on disk
-sgdisk -a 2048 -o ${DISK}   # new gpt disk 2048 alignment
+sgdisk -Z ${DISK}           # destroying existing mbr/gpt structures on disk
+sgdisk -a 2048 -o ${DISK}   # creating new gpt disk 2048 alignment
 
 echo "------------------------------------------"
 echo "--          Creating partitions         --"
@@ -54,9 +70,9 @@ mount "${DISK}p2" /mnt          # mounting second partition
 mount "${DISK}p3" /mnt/home/    # mounting third partition 
 
 
-echo "--------------------------------------"
-echo "--        Arch Base Install         --"
-echo "--------------------------------------"
+echo "------------------------------------------"
+echo "--         Installing base Arch         --"
+echo "------------------------------------------"
 pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware git --noconfirm
 genfstab -U /mnt >> /mnt/etc/fstab
 
@@ -65,3 +81,62 @@ echo "------------------------------------------"
 echo "--    Changing root directory to /mnt   --"
 echo "------------------------------------------"
 arch-chroot /mnt
+
+
+echo "------------------------------------------"
+echo "--        Setting up root password      --"
+echo "------------------------------------------"
+passwd
+
+
+echo "------------------------------------------"
+echo "--            Installing GRUB           --"
+echo "------------------------------------------"
+pacman -S grub efibootmgr amd-ucode --noconfirm  # that may not be appropriate, depending on your CPU manufacturer
+lsblk
+echo "Please enter disk to install grub on: (probably /dev/sda)"
+read DISK
+grub-install "${DISK}"
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "------------------------------------------"
+echo "--          Setting up network          --"
+echo "------------------------------------------"
+pacman -S networkmanager --noconfirm --needed
+systemctl enable NetworkManager
+
+
+echo "------------------------------------------"
+echo "--          Setting up firewall         --"
+echo "------------------------------------------"
+pacman -S ufw --noconfirm --needed
+ufw default deny incoming    # setting up sensible ufw default rules 
+ufw default allow outgoing
+systemctl enable ufw
+ufw enable
+
+
+echo "------------------------------------------"
+echo "--      Downloading basic software      --"
+echo "------------------------------------------"
+pacman -S git nano vim bash-completion --noconfirm --needed
+
+
+echo "------------------------------------------"
+echo "--       Setting up locale things       --"
+echo "--       Language: English              --"
+echo "--       Keyboard: de-latin1            --"
+echo "--       Timezone: Berlin, Germany      --"
+echo "------------------------------------------"
+echo KEYMAP=de-latin1 > /etc/vconsole.conf  # setting de-latin1 as keyboard layout 
+echo ArchLinux >> /etc/hostname             # setting hostname
+echo '127.0.0.1   localhost' >> /etc/hosts  # setting loopback address
+echo '::1   localhost' >> /etc/hosts
+timedatectl set-timezone Europe/Berlin      # setting timezone 
+timedatectl set-ntp true                    # enabling NTP
+timedatectl status                          # displaying status of the time
+nano /etc/locale.gen | sed -e "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8"  # setting up language to English 
+echo LANG=en_US.UTF-8 > /etc/locale.conf    
+locale-gen
+sed -e "s/kernel.sysrq = 16/kernel.sysrq = 1/g" /usr/lib/sysctl.d/50-default.conf  # enabling all kernel level shortcuts
